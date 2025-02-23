5426d1906e5823b307665271dbfe36ab1cd5e44d
Jonathan Emery <JonathanREmery@gmail.com>
2025-02-23 13:45:21 +0700

    chore(stack): added more comments

--- stack/stack.c
+++ stack/stack.c
@@ -7,14 +7,17 @@
  * @return Pointer to the stack
  */
 stack_t* stack_create(uint64_t element_size, uint64_t stack_size) {
+    // Allocate memory for the stack structure
     stack_t* stack = (stack_t*)malloc(sizeof(stack_t));
 
+    // Initialize the stack
     stack->element_size = element_size;
     stack->stack_size = stack_size;
     stack->elements = 0;
     stack->base = (void*)malloc(stack_size * element_size);
     stack->top = stack->base;
 
+    // Return the stack
     return stack;
 }
 
@@ -23,8 +26,10 @@ stack_t* stack_create(uint64_t element_size, uint64_t stack_size) {
  * @param stack Pointer to the stack
  */
 void stack_grow(stack_t* stack) {
+    // Double the stack size
     stack->stack_size *= 2;
 
+    // Reallocate the stack
     stack->base = realloc(stack->base, stack->stack_size * stack->element_size);
     stack->top = stack->base + stack->elements * stack->element_size;
 }
@@ -35,10 +40,12 @@ void stack_grow(stack_t* stack) {
  * @param value Value to push onto the stack
  */
 void stack_push(stack_t* stack, void* value) {
+    // If the stack is full, grow it
     if (stack->elements == stack->stack_size) {
         stack_grow(stack);
     }
 
+    // Copy the value to the top of the stack
     memcpy(stack->top, value, stack->element_size);
     stack->top += stack->element_size;
     stack->elements++;
@@ -50,12 +57,16 @@ void stack_push(stack_t* stack, void* value) {
  * @return Pointer to the element, NULL if the stack is empty
  */
 void* stack_pop(stack_t* stack) {
+    // If the stack is empty, return NULL
     if (stack->top == stack->base) {
         return NULL;
     }
 
+    // Pop the value from the stack
     stack->top -= stack->element_size;
     stack->elements--;
+
+    // Return the value
     return stack->top;
 }
 
@@ -65,10 +76,12 @@ void* stack_pop(stack_t* stack) {
  * @return Pointer to the element, NULL if the stack is empty
  */
 void* stack_peek(stack_t* stack) {
+    // If the stack is empty, return NULL
     if (stack->top == stack->base) {
         return NULL;
     }
 
+    // Return the value
     return stack->top - stack->element_size;
 }
 
@@ -76,6 +89,7 @@ void* stack_peek(stack_t* stack) {
  * Destroy the stack
  */
 void stack_destroy(stack_t* stack) {
+    // Free the memory allocated for the stack
     free(stack->base);
     free(stack);
 }
